302221ee7625a33cea4f25ccc5ba5676bb2022d9
임진영 <delheure29@gmail.com>
2021-01-13 10:54:29 +0900

    Add : Nav

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SignUp.scss";
+
+export class SignUp extends React.Component {
+  render() {
+    return (
+      <>
+        <section class="SignUp">회원가입</section>
+      </>
+    );
+  }
+}
+
+export default SignUp;
+
