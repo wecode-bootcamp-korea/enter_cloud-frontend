18d1f7a94c77a33fc9770dfede39dd880076c512
임진영 <delheure29@gmail.com>
2021-01-13 13:53:29 +0900

    add: Nav, slideMenu

---
+++ src/Components/SlideMenu/SlideMenu.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SlideMenu.scss";
+
+export class SlideMenu extends React.Component {
+  render() {
+    return <button class="btn_menu btn_com">menu bar</button>;
+  }
+}
+
+export default SlideMenu;
