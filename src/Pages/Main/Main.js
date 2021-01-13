1bc312b8dc81bde426b107eabd12c8e894571fee
문규찬 <lazychan90@gmail.com>
2021-01-13 16:05:08 +0900

    [16기 문규찬] MainPage/Footer 레이아웃 구현중

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./Main.scss";
 import "react-router-dom";
-import Slide from "./Components/Slide";
+import Slide from "./Components/Slide/Slide";
+import Footer from "./Components/Footer/Footer";
 
 export class Main extends React.Component {
   render() {
     return (
-      <div className="Main">
+      <div className="main">
         <Slide />
-        <section className="Main_category">
-          <div className="Main_category_wrap1">
+        <section className="main_category">
+          <div className="main_category_wrap1">
             <span>어떤 공간을 찾고 있나요?</span>
             <ul>
               <li>1</li>
               <li>2</li>
             </ul>
           </div>
-          <div className="Main_category_wrap2">
+          <div className="main_category_wrap2">
             <span>오늘의 추천공간</span>
             <ul>
               <li>1</li>
@@ -24,14 +25,15 @@ export class Main extends React.Component {
             </ul>
           </div>
         </section>
-        <section className="Main_review">
-          <div className="Main_review_wrap">
+        <section className="main_review">
+          <div className="main_review_wrap">
             <h2>이용자 리뷰</h2>
             <h3>생생한 후기를 만나보세요</h3>
-            <div className="Main_border">보더</div>
+            <div className="main_border">보더</div>
             <button>더보기</button>
           </div>
         </section>
+        <Footer />
       </div>
     );
   }
