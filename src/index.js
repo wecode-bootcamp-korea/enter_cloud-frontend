7a303064adbd0c5a4ff02b29bb70675f73c5c804
김승완 <reddht721@gmail.comlspwdcdgit config --global user.email reddht721@gmail.com>
2021-01-18 11:54:32 +0900

    main에서 pull받기

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
-import Routes from "./Routes";
-import "./Styles/Common.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import Routes from './Routes';
+import './Styles/Common.scss';
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(<Routes />, document.getElementById('root'));
 reportWebVitals();
