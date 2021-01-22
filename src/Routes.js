44a2b7bd606cedaeb2ad7fef0fb104807ae75c4e
김승완 <reddht721@gmail.comlspwdcdgit config --global user.email reddht721@gmail.com>
2021-01-22 14:27:57 +0900

    1차프로젝트 우선최종

--- src/Routes.js
+++ src/Routes.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Main } from './Pages/Main/Main';
-import { Login } from './Pages/Login/Login';
-import { SignUp } from './Pages/SignUp/SignUp';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
 import Details from './Pages/Details/Details';
 import { Lists } from './Pages/Lists/Lists';
 import Nav from './Components/Nav/Nav';
